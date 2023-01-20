b5ea9f9af048adc34bdfcccd70c87ab52430f284
Waldemar Hermann <hermann.waldemar19995@gmail.com>
2023-01-20 23:50:41 +0100

    add Navigation component

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,12 @@
+
+
+const Navigation = () => {
+    return (
+        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <p className="f3 link dim black underlined pa3 pointer">Sign Out</p>
+
+        </nav>
+    )
+}
+
+export default Navigation;
