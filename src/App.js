f198b1e05ad857697957f88a6f33b03b0f6f8ebc
Waldemar Hermann <hermann.waldemar19995@gmail.com>
2023-01-20 23:49:41 +0100

    create class App

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { Component } from 'react';
+import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        <Logo />
+        {/* <ImageLinkForm />
+        <FaceRecognitation /> */}
+      </div>
+    );
+  }
+}
+
+export default App;
