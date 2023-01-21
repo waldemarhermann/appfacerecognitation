16a1110d7350e817a3f1855da00818f2e99777aa
Waldemar Hermann <hermann.waldemar19995@gmail.com>
2023-01-21 14:58:27 +0100

    add components

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import { Component } from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank.js/Rank';
+import ParticlesBg from 'particles-bg';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <ParticlesBg type="color" bg={true} />
         <Navigation />
         <Logo />
-        {/* <ImageLinkForm />
-        <FaceRecognitation /> */}
+        < Rank />
+        <ImageLinkForm />
+        
+        {/* <FaceRecognitation /> */}
       </div>
     );
   }
