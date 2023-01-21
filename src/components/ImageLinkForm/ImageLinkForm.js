354db2f63ba7c4c29574e42e25f084fde4b111ca
Waldemar Hermann <hermann.waldemar19995@gmail.com>
2023-01-21 14:59:31 +0100

    create ImageLinkForm component

---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,17 @@
+import './ImageLinkForm.css';
+
+const ImageLinkForm = () => {
+    return (
+        <div>
+            <p className="f3"> {'This programm is able to find faces on any pictures'} </p>
+            <div className='center' >
+                <div className="center form pa4 br3 shadow-5">
+                <input className="f4 pa2 w-70" type="tex" />
+                <button className="f4 w-30 grow link ph3 pv2 dib white bg-green">Click</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ImageLinkForm;
