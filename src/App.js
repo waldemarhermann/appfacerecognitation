123bea2d208166635828c947376368e3c8302596
Waldemar Hermann <hermann.waldemar19995@gmail.com>
2023-01-21 17:05:39 +0100

    add constructor, this.state

--- src/App.js
+++ src/App.js
@@ -8,6 +8,21 @@ import ParticlesBg from 'particles-bg';
 
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value)
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+  }
+
   render() {
     return (
       <div className="App">
@@ -15,7 +30,7 @@ class App extends Component {
         <Navigation />
         <Logo />
         < Rank />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
         
         {/* <FaceRecognitation /> */}
       </div>
