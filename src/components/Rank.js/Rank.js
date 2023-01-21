1d20a3640bb8b8aa815ef795e3a449db0e21685f
Waldemar Hermann <hermann.waldemar19995@gmail.com>
2023-01-21 15:01:05 +0100

    create component

---
+++ src/components/Rank.js/Rank.js
@@ -0,0 +1,15 @@
+
+const Rank = () => {
+    return (
+        <div>
+            <div className="f3 white">
+                {'Wowa, you are at position '}
+            </div>
+            <div className="f1 white">
+                {'#5'}
+            </div>
+        </div>
+    )
+}
+
+export default Rank;
