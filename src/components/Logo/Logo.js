7d3e6204a9f4d4442bf8d9dfccb92f7e1050dc49
Waldemar Hermann <hermann.waldemar19995@gmail.com>
2023-01-21 15:00:57 +0100

    completed component

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,7 +1,15 @@
-const Logo = () => {
-    return(
-        <div>
+import Tilt from 'react-parallax-tilt';
+import './Logo.css';
+import Kibrain from './Kibrain.png';
 
+const Logo = () => {
+    return (
+        <div className="ma4 mt 0">
+            <Tilt className='Tilt br2 shadow-2' options={{max: 40}} style={{height: 150, width: 150}}>
+                <div className='Tilt-inner pa3'>
+                    <img style={{paddingTop: '10px'}} src={Kibrain} alt="img" />
+                </div>
+            </Tilt>
         </div>
     )
 }
