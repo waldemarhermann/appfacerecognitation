0f8975f7eb0ddfa9df1a7b1c34919514baabbb45
Waldemar Hermann <hermann.waldemar19995@gmail.com>
2023-01-21 17:06:09 +0100

    add props, onclick, onchange function

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,13 +1,16 @@
 import './ImageLinkForm.css';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = (props) => {
     return (
         <div>
             <p className="f3"> {'This programm is able to find faces on any pictures'} </p>
             <div className='center' >
                 <div className="center form pa4 br3 shadow-5">
-                <input className="f4 pa2 w-70" type="tex" />
-                <button className="f4 w-30 grow link ph3 pv2 dib white bg-green">Click</button>
+                <input className="f4 pa2 w-70" type="tex" onChange={props.onInputChange}/>
+                <button 
+                className="f4 w-30 grow link ph3 pv2 dib white bg-green"
+                onClick={props.onButtonSubmit}
+                >Click</button>
                 </div>
             </div>
         </div>
