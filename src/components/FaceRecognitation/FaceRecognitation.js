72bd50523d9f844490c3ce709f4c808e192cf255
Waldemar Hermann <hermann.waldemar19995@gmail.com>
2023-01-22 21:40:03 +0100

    add FaceRecognitation component

---
+++ src/components/FaceRecognitation/FaceRecognitation.js
@@ -0,0 +1,9 @@
+const FaceRecognitation = (props) => {
+    return(
+        <div className="center">
+            <img src={props.imageUrl} alt="img" />
+        </div>
+    )
+}
+
+export default FaceRecognitation;
