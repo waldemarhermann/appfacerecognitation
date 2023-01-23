5ab4240fea8a06fc75e2fcb9b8c60d82f721da62
Waldemar Hermann <hermann.waldemar19995@gmail.com>
2023-01-23 14:47:14 +0100

    #

--- src/components/FaceRecognitation/FaceRecognitation.js
+++ src/components/FaceRecognitation/FaceRecognitation.js
@@ -1,8 +1,12 @@
+import React from 'react';
+import './FaceRecognitation.css';
+
 const FaceRecognitation = (props) => {
     return(
         <div className="center ma">
             <div className="absolute mt2">
-            <img src={props.imageUrl} alt="img" width='500px' height='auto' />
+            <img id="inputimage" src={props.imageUrl} alt="img" width='500px' height='auto' />
+            <div className="bounding-box" style={{top: props.box.topRow , left: props.box.leftCol, right: props.box.rightCol, bottom: props.box.bottomRow}}></div>
             </div>
         </div>
     )
