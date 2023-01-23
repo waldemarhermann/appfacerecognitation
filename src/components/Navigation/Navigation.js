85e7e9d4cfc59bdc70df29ced7dd532f05795af8
Waldemar Hermann <hermann.waldemar19995@gmail.com>
2023-01-23 14:47:31 +0100

    #

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,10 +1,9 @@
-
+import React from "react";
 
 const Navigation = () => {
     return (
         <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
             <p className="f3 link dim black underlined pa3 pointer">Sign Out</p>
-
         </nav>
     )
 }
