8bd2cb67500ff2bc9d81cb750df481c2482a1b44
Waldemar Hermann <hermann.waldemar19995@gmail.com>
2023-01-28 23:13:10 +0100

    add onClick function

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Navigation = () => {
+const Navigation = (props) => {
     return (
-        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-            <p className="f3 link dim black underlined pa3 pointer">Sign Out</p>
+        <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
+            <p onClick={() => props.onRouteChange('signin')} className="f3 link dim black underlined pa3 pointer">Sign Out</p>
         </nav>
     )
 }
