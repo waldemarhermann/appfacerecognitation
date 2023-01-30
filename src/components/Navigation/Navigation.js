50ba486f6fc0f122f04673725fcdd93f3696ad26
Waldemar Hermann <hermann.waldemar19995@gmail.com>
2023-01-30 22:01:20 +0100

    add further nav-element

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,20 @@
 import React from "react";
 
 const Navigation = (props) => {
-    return (
-        <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
-            <p onClick={() => props.onRouteChange('signin')} className="f3 link dim black underlined pa3 pointer">Sign Out</p>
-        </nav>
-    )
+    if (props.isSignedIn) {
+        return (
+            <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                <p onClick={() => props.onRouteChange('signout')} className="f3 link dim black underlined pa3 pointer">Sign Out</p>
+            </nav>
+        )
+    } else {
+        return (
+            <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+                <p onClick={() => props.onRouteChange('signin')} className="f3 link dim black underlined pa3 pointer">Sign In</p>
+                <p onClick={() => props.onRouteChange('register')} className="f3 link dim black underlined pa3 pointer">Register</p>
+            </nav>
+        )
+    }
 }
 
 export default Navigation;
